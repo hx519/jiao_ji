ccf0a7fb54dd02fc6cc197a9bb36c12a8ea1feb8
hx519 <125659578+hx519@users.noreply.github.com>
2023-05-18 10:56:23 +0800

    Delete JiaoJi_web-main directory

--- JiaoJi_web-main/jiaoji_app_back/jiaoji_app_back/src/main/java/com/example/jiaoji_app_back/dao/SignupDao.java
+++
@@ -1,5 +0,0 @@
-package com.example.jiaoji_app_back.dao;
-
-public interface SignupDao {
-    boolean SignUp(Integer userID, Integer actId);
-}
--- JiaoJi_web-main/jiaoji_app_back/jiaoji_app_back/src/main/java/com/example/jiaoji_app_back/service/SignUpService.java
+++
@@ -1,9 +0,0 @@
-package com.example.jiaoji_app_back.service;
-
-import com.example.jiaoji_app_back.entity.ActivitySignup;
-
-public interface SignUpService {
-
-    boolean SignUp(Integer userID, Integer actId);
-
-}
